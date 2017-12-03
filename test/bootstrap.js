75959cbe5144f095f15f629507bd66d799f62e76
albertosouza <alberto.souza.99@gmail.com>
2017-12-03 15:44:03 -0200

    remove uneed modules from tests

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -1,25 +1,8 @@
 var projectPath = process.cwd();
 var deleteDir = require('rimraf');
 var testTools = require('we-test-tools');
-var ncp = require('ncp').ncp;
-var mkdirp = require('mkdirp');
 var path = require('path');
 var we;
-
-// before(function(callback) {
-//   var dest = path.resolve(process.cwd(), 'server/emails');
-
-//   mkdirp(dest, function(){
-//     ncp(
-//       path.resolve(__dirname, 'stubs/emails'),
-//       dest, function (err) {
-//       if (err) {
-//         return callback(err);
-//       }
-//       callback();
-//     });
-//   })
-// });
 
 before(function(callback) {
   this.slow(100);
