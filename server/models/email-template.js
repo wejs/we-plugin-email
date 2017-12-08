08674d2a0e64a2a0e684936c5728bec9facb457e
albertosouza <alberto.souza.99@gmail.com>
2017-12-08 00:29:05 -0200

    add support for default subject, html and text

--- server/models/email-template.js
+++ server/models/email-template.js
@@ -26,6 +26,16 @@ module.exports = function emailTemplateModel(we) {
       type: {
         type: we.db.Sequelize.STRING,
         allowNull: true
+      },
+      typeSettings: {
+        type: we.db.Sequelize.VIRTUAL,
+        get() {
+          if (!this.type) {
+            return {};
+          }
+
+          return we.config.emailTypes[ this.type ] || {};
+        }
       }
     },
 
@@ -34,18 +44,25 @@ module.exports = function emailTemplateModel(we) {
       instanceMethods: {
         render(templateVariables) {
           return new Promise( (resolve)=> {
+            const typeSettings = this.typeSettings;
             let result = {};
 
             if (this.subject) {
               result.subject = _.template(this.subject)(templateVariables);
+            } else if (typeSettings.defaultSubject) {
+              result.subject = _.template(typeSettings.defaultSubject)(templateVariables);
             }
 
             if (this.text) {
               result.text = _.template(this.text)(templateVariables);
+            } else if (typeSettings.defaultText) {
+              result.text = _.template(typeSettings.defaultSubject)(templateVariables);
             }
 
             if (this.html) {
               result.html = _.template(this.html)(templateVariables);
+            } else if (typeSettings.defaultHTML) {
+              result.html = _.template(typeSettings.defaultHTML)(templateVariables);
             }
 
             resolve(result);
